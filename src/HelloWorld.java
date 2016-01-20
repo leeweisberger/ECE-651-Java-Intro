9c7b703e88f821c71ed9126509f33bbc6659de72
leeweisberger <lee.weisberger@duke.edu>
2016-01-19 21:49:04 -0500

    updated hellow world with static non static examples

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,20 @@
 
 public class HelloWorld {
+	public static final String HELLO_WORLD= "Hello World";
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println(HELLO_WORLD);
+		System.out.println(helloWorldStatic());
+		
+		HelloWorld hw = new HelloWorld();
+		System.out.println(hw.helloWorldNonStatic());
+	}
+	
+	private static String helloWorldStatic(){
+		return HELLO_WORLD;
+	}
+	
+	private String helloWorldNonStatic(){
+		return HELLO_WORLD;
 	}
 }
